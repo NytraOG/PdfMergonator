8396e9f6ace508fbae13bbe26e7a5d626b6f4bd3
Christian Nytra <christian.nytra@co-orga.d>
2023-06-18 16:38:19 +0200

    erster import fertig

--- Main/Models/MainViewModel.cs
+++ Main/Models/MainViewModel.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Microsoft.Win32;
 
 namespace Main.Models;
 
 public class MainViewModel : BaseViewModel
 {
-    private ICommand? testCommand;
-    private string    testText = "hihi";
+    private ICommand? clearCommand;
+    private ICommand? importFilesCommand;
+    private string    pdfTextContent = "Empty";
 
     public MainViewModel(List<string> arguments)
     {
@@ -18,23 +24,61 @@ public class MainViewModel : BaseViewModel
     public PdfPageViewModel PdfPageViewModel { get; }
     public List<string>     Arguments        { get; }
 
-    public string TestText
+    public string PdfTextContent
     {
-        get => testText;
+        get => pdfTextContent;
         set
         {
-            testText = value;
+            pdfTextContent = value;
             OnPropertyChanged();
         }
     }
 
-    public ICommand TestCommand => testCommand ??= new CommandHandler(async () =>
+    public ICommand ClearCommand => clearCommand ??= new CommandHandler(async () =>
     {
-        await Task.Delay(2000);
-
         await Task.Run(() =>
-        {
-            TestText = "Hupu";
-        });
+                   {
+                       PdfTextContent = string.Empty;
+                   })
+                  .ConfigureAwait(false);
+    }, () => true);
+
+    public ICommand ImportFilesCommand => importFilesCommand ??= new CommandHandler(async () =>
+    {
+        await Task.Run(() =>
+                   {
+                       var dialog = new OpenFileDialog
+                       {
+                           DefaultExt  = ".pdf",
+                           Filter      = "Pdf Files (.pdf)|*.pdf",
+                           Multiselect = true
+                       };
+
+                       var result = dialog.ShowDialog();
+
+                       if (result != true)
+                           return;
+
+                       var fileNames = dialog.FileNames;
+
+                       foreach (var fileName in fileNames)
+                       {
+                           if (dialog.CheckFileExists)
+                           {
+                               var reader = new PdfReader(fileName);
+
+                               for (var i = 1; i <= reader.NumberOfPages; i++)
+                               {
+                                   var content          = PdfTextExtractor.GetTextFromPage(reader, i, new LocationTextExtractionStrategy());
+                                   var formattedContent = Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(content)));
+
+                                   PdfTextContent += Environment.NewLine + Environment.NewLine + formattedContent;
+                               }
+
+                               reader.Close();
+                           }
+                       }
+                   })
+                  .ConfigureAwait(false);
     }, () => true);
 }
